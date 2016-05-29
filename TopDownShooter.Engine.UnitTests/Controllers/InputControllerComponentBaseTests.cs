15f56feb688f8823c16cafea9476fed549dca52c
unknown <ShandsJM@DEV-JSHANDSLT.develop.ds.amrdec.army.mil>
2016-05-29 16:01:53 -0500

    Added unit tests for InputControllerComponentBase class.

---
+++ TopDownShooter.Engine.UnitTests/Controllers/InputControllerComponentBaseTests.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InputControllerComponentBaseTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TopDownShooter.Engine.UnitTests.Controllers
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.Xna.Framework;
+    using Moq;
+    using TopDownShooter.Engine.Controllers;
+
+    /// <summary>
+    /// Contains unit tests for the <see cref="InputControllerComponentBase" /> class.
+    /// </summary>
+    [TestClass]
+    public class InputControllerComponentBaseTests
+    {
+        /// <summary>
+        /// Tests that the fire message is broadcasted when fire is pressed.
+        /// </summary>
+        [TestMethod]
+        public void BroadcastsFireMessageWhenFireIsPressedAndUpdated()
+        {
+            bool wasBroadcastMessageCalled = false;
+            var gameObject = new Mock<IGameObject>();
+            gameObject.Setup(go => go.BroadcastMessage(It.IsAny<ComponentMessage>())).Callback<ComponentMessage>(message =>
+                {
+                    wasBroadcastMessageCalled = true;
+                    Assert.AreEqual(MessageType.Fire, message.MessageType);
+                });
+
+            var uut = new TestInputControllerComponent();
+            uut.Update(gameObject.Object, new GameTime());
+            Assert.IsTrue(wasBroadcastMessageCalled);
+        }
+    }
+}
---
+++ TopDownShooter.Engine.UnitTests/Controllers/TestInputControllerComponent.cs
@@ -0,0 +1,72 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TestInputControllerComponent.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TopDownShooter.Engine.UnitTests.Controllers
+{
+    using TopDownShooter.Engine.Controllers;
+
+    /// <summary>
+    /// Defines a test <see cref="InputControllerComponentBase" /> class.
+    /// </summary>
+    public class TestInputControllerComponent : InputControllerComponentBase
+    {
+        public override void Destroy()
+        {
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether a fire was requested.
+        /// </summary>
+        /// <returns>True if the action was requested; false otherwise.</returns>
+        public override bool Fire()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Initializes the component.
+        /// </summary>
+        public override void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// Gets whether or not a up move was requested.
+        /// </summary>
+        /// <returns>True to move down.</returns>
+        public override bool MoveDown()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets whether or not a left move was requested.
+        /// </summary>
+        /// <returns>True to move left.</returns>
+        public override bool MoveLeft()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets whether or not a right move was requested.
+        /// </summary>
+        /// <returns>True to move right.</returns>
+        public override bool MoveRight()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets whether or not a up move was requested.
+        /// </summary>
+        /// <returns>True to move up.</returns>
+        public override bool MoveUp()
+        {
+            return false;
+        }
+    }
+}
